a7d57389ca0c4c4586127c3875cb369d7bf8aee3
Victoria Lim <lim.t.victoria@gmail.com>
2017-02-27 16:40:08 -0800

    scripts for F182 rotation

---
+++ 07_rotateF182/01_setup/namd-multicore.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#$ -N dihed180
+#$ -q tw,pub64,free64
+# $ -q free64
+#$ -pe openmp 64
+#$ -ckpt blcr
+
+
+# Load the Multicore (OpenMP) NAMD version 2.11:
+module load  NAMD-multicore/2.11
+
+# We define $CORES in Grid Engine to make life easier.  The $CORES
+# environment variable containts the number of cores allocated
+# to this job.
+
+# Replace the input / output files:
+charmrun +p $CORES  `which namd2` equil1.inp > out1.log
