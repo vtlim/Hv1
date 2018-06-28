35b3356961f6bfc714cbf2000da756635cd79f8c
Victoria Lim <lim.t.victoria@gmail.com>
2018-06-28 11:04:33 -0700

    major modifications to VMD view script

--- 02_configs/viewer.tcl
+++ 02_configs/viewer.tcl
@@ -1,55 +1,134 @@
 
-# After loading psf and dcd, use this script to view GBI, 4 residues, Hv1.
-# "source file.tcl"
+# ============================================================================
+# After loading psf and dcd, use this script for various visualizations of Hv1.
+# Only modifies visualization on TOP molecule.
+# Steps:
+#   1. "source file.tcl"
+#   2. "view_clear"
+#   3. "view_[four/protein]"
+# By: Victoria Lim
+# ============================================================================
 
 #set psf [glob *psf]
 #mol new $psf type {psf} first 0 last -1 step 1 waitfor all
-#mol addfile npt_eq01.dcd type {dcd} first 0 last -1 step 1 waitfor -1 $count
+#mol addfile npt_eq01.dcd type {dcd} first 0 last -1 step 1 waitfor -1 $moltop
 
+
+# VMD display settings
 color Display Background white
 display projection Orthographic
 display depthcue off
+axes location LowerLeft
+set repcount 1
 
-set count 0
-mol delrep 0 $count
+proc view_clear {} {
+    foreach moltop [molinfo list] {
+        # assumes no more than 20 representations drawn
+        for {set i 0} {$i < 20} {incr i} {
+            mol delrep 0 $moltop
+        }
+    }
+    set ::repcount 0
+}
 
-# add representation for file with GBI only
-mol addrep $count
-mol modselect 0 $count resname GBI1
-mol modstyle 0 $count Licorice
-mol modcolor 0 $count ColorID 4
+proc view_protein {} {
+    global repcount
+    set moltop [molinfo top]
 
-# add representation for protein
-mol addrep $count
-mol modselect 1 $count protein
-mol modstyle 1 $count NewCartoon
-mol modcolor 1 $count ColorID 0
-mol modmaterial 1 $count Transparent
+    # add representation for file with GBI only
+    mol addrep $moltop
+    mol modselect $repcount $moltop resname GBI1
+    mol modstyle $repcount $moltop Licorice
+    mol modcolor $repcount $moltop ColorID 4
+    incr ::repcount
 
-# add representation for residues
-mol addrep $count
-mol modselect 2 $count protein and resid 112
-mol modstyle 2 $count Licorice 0.300000 12.000000 12.000000
-mol modcolor 2 $count ColorID 10
+    # add representation for protein
+    mol addrep $moltop
+    mol modselect $repcount $moltop protein
+    mol modstyle $repcount $moltop NewCartoon
+    mol modcolor $repcount $moltop ColorID 6
+    #mol modmaterial 1 $moltop Transparent
+    incr ::repcount
 
-# add representation for residues
-mol addrep $count
-mol modselect 3 $count protein and resid 150
-mol modstyle 3 $count Licorice 0.300000 12.000000 12.000000
-mol modcolor 3 $count ColorID 3
+    # add representation for S1
+    mol addrep $moltop
+    mol modselect $repcount $moltop protein and resid 99 to 125
+    mol modstyle $repcount $moltop NewCartoon
+    mol modcolor $repcount $moltop ColorID 10
+    incr ::repcount
 
-# add representation for residues
-mol addrep $count
-mol modselect 4 $count protein and resid 181
-mol modstyle 4 $count Licorice 0.300000 12.000000 12.000000
-mol modcolor 4 $count ColorID 7
+    # add representation for S2
+    mol addrep $moltop
+    mol modselect $repcount $moltop protein and resid 134 to 160
+    mol modstyle $repcount $moltop NewCartoon
+    mol modcolor $repcount $moltop ColorID $repcount
+    incr ::repcount
 
-# add representation for residues
-mol addrep $count
-mol modselect 5 $count protein and resid 211
-mol modstyle 5 $count Licorice 0.300000 12.000000 12.000000
-mol modcolor 5 $count ColorID 11
+    # add representation for S3
+    mol addrep $moltop
+    mol modselect $repcount $moltop protein and resid 168 to 191
+    mol modstyle $repcount $moltop NewCartoon
+    mol modcolor $repcount $moltop ColorID 7
+    incr ::repcount
 
-# hide displays for ease of viewing
-#mol off $count           ;# don't display anything to start
-#mol showrep $count 1 0   ;# don't show protein
+    # add representation for S4
+    mol addrep $moltop
+    mol modselect $repcount $moltop protein and resid 198 to 222
+    mol modstyle $repcount $moltop NewCartoon
+    mol modcolor $repcount $moltop ColorID 11
+    incr ::repcount
+
+}
+
+proc view_four {} {
+    global repcount
+    set moltop [molinfo top]
+
+    # add representation for file with GBI only
+    mol addrep $moltop
+    mol modselect $repcount $moltop resname GBI1
+    mol modstyle $repcount $moltop Licorice
+    mol modcolor $repcount $moltop ColorID 4
+    incr ::repcount
+
+    # add represe$repcount ntation for protein
+    mol addrep $moltop
+    mol modselect $repcount $moltop protein
+    mol modstyle $repcount $moltop NewCartoon
+    mol modcolor $repcount $moltop ColorID 0
+    mol modmaterial $repcount $moltop Transparent
+    incr ::repcount
+
+    # add represe$repcount ntation for residues
+    mol addrep $moltop
+    mol modselect $repcount $moltop protein and resid 112
+    mol modstyle $repcount $moltop Licorice 0.300000 12.000000 12.000000
+    mol modcolor $repcount $moltop ColorID 10
+    incr ::repcount
+
+    # add represe$repcount ntation for residues
+    mol addrep $moltop
+    mol modselect $repcount $moltop protein and resid 150
+    mol modstyle $repcount $moltop Licorice 0.300000 12.000000 12.000000
+    mol modcolor $repcount $moltop ColorID 3
+    incr ::repcount
+
+    # add represe$repcount ntation for residues
+    mol addrep $moltop
+    mol modselect $repcount $moltop protein and resid 181
+    mol modstyle $repcount $moltop Licorice 0.300000 12.000000 12.000000
+    mol modcolor $repcount $moltop ColorID 7
+    incr ::repcount
+
+    # add represe$repcount ntation for residues
+    mol addrep $moltop
+    mol modselect $repcount $moltop protein and resid 211
+    mol modstyle $repcount $moltop Licorice 0.300000 12.000000 12.000000
+    mol modcolor $repcount $moltop ColorID 11
+    incr ::repcount
+
+    # hide displays for ease of viewing
+    #mol off $moltop           ;# don't display anything to start
+    #mol showrep $moltop 1 0   ;# don't show protein
+}
+
