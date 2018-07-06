2fd03ccb8d8e16ee4316e56299b343100660f732
Victoria Lim <lim.t.victoria@gmail.com>
2018-07-06 10:14:45 -0700

    more generalizable ligHbonds script

--- 04_fep/postFEPvanilla/analysis/3_ligClose/ligHbonds.tcl
+++ 04_fep/postFEPvanilla/analysis/3_ligClose/ligHbonds.tcl
@@ -1,10 +1,19 @@
 
-# usage `vmd -e ligHbonds.tcl -args t1_d112e`
-# not vmdt because plugins don't get loaded
+# usage `vmdt -e ligHbonds.tcl -args mypsf mydcd resname,GBI2`
 
-set key [lindex $argv 0]
+package require pbctools
+package require hbonds
 
-mol new $key.psf
-mol addfile $key.dcd first 0 last -1 step 100 waitfor all 
+set mypsf [lindex $argv 0]
+set mydcd [lindex $argv 1]
+set prelig [lindex $argv 2]
 
-hbonds -sel1 [atomselect top protein] -sel2 [atomselect top "resname GBI1"] -writefile yes -upsel yes -frames all -dist 3.5 -ang 40 -plot yes -log ${key}_hbonds.log -writefile yes -outfile ${key}_hbonds.dat -polar yes -DA both -type unique -detailout ${key}_hbonds-details.dat
+set mylig [split $prelig {,}]
+
+mol new $mypsf
+mol addfile $mydcd first 0 last -1 step 10 waitfor all 
+pbc wrap -compound fragment -center com -centersel "$mylig" -all
+
+hbonds -sel1 [atomselect top "protein or water"] -sel2 [atomselect top "$mylig"] -writefile yes -upsel yes -frames all -dist 3.5 -ang 40 -plot yes -log hbondsProtWat.log -writefile yes -outfile hbondsProtWat.dat -polar yes -DA both -type unique -detailout hbondsProtWat-details.dat
+
+exit
