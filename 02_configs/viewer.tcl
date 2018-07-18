5b2715edf88af5aec2a8362c40d837efba51c4eb
Victoria Lim <lim.t.victoria@gmail.com>
2018-07-18 14:54:56 -0700

    add new proc for hbond network viewing

--- 02_configs/viewer.tcl
+++ 02_configs/viewer.tcl
@@ -103,6 +103,41 @@ proc view_protein {} {
 
 }
 
+
+proc view_network { {lig "resname GBI1"} } {
+    global repcount
+    set moltop [molinfo top]
+
+    # the ligand
+    mol color Name
+    mol representation Licorice 0.200000 10.000000 10.000000
+    mol selection "$lig"
+    mol material Opaque
+    mol addrep $moltop
+    mol selupdate $repcount $moltop 1 ;# traj update not applied if repcount gets reset
+    incr ::repcount
+
+    # the neighbors
+    mol color ResType
+    mol representation Licorice 0.100000 10.000000 10.000000
+    mol selection "(protein or water) and same residue as noh and within 5 of $lig"
+    mol material Opaque
+    mol addrep $moltop
+    mol selupdate $repcount $moltop 1
+    incr ::repcount
+
+    # the hbond network
+    mol color Name
+    mol representation HBonds 3.500000 40.000000 6.000000
+    mol selection "(nitrogen or oxygen) and ($lig or (protein or water) and same residue as noh and within 5 of $lig)"
+    mol material Opaque
+    mol addrep $moltop
+    mol selupdate $repcount $moltop 1
+    incr ::repcount
+
+}
+
+
 proc view_four {} {
     global repcount
     set moltop [molinfo top]
@@ -114,7 +149,7 @@ proc view_four {} {
     mol modcolor $repcount $moltop ColorID 4
     incr ::repcount
 
-    # add represe$repcount ntation for protein
+    # add representation for protein
     mol addrep $moltop
     mol modselect $repcount $moltop protein
     mol modstyle $repcount $moltop NewCartoon
@@ -122,28 +157,28 @@ proc view_four {} {
     mol modmaterial $repcount $moltop Transparent
     incr ::repcount
 
-    # add represe$repcount ntation for residues
+    # add representation for residues
     mol addrep $moltop
     mol modselect $repcount $moltop protein and resid 112
     mol modstyle $repcount $moltop Licorice 0.300000 12.000000 12.000000
     mol modcolor $repcount $moltop ColorID 10
     incr ::repcount
 
-    # add represe$repcount ntation for residues
+    # add representation for residues
     mol addrep $moltop
     mol modselect $repcount $moltop protein and resid 150
     mol modstyle $repcount $moltop Licorice 0.300000 12.000000 12.000000
     mol modcolor $repcount $moltop ColorID 3
     incr ::repcount
 
-    # add represe$repcount ntation for residues
+    # add representation for residues
     mol addrep $moltop
     mol modselect $repcount $moltop protein and resid 181
     mol modstyle $repcount $moltop Licorice 0.300000 12.000000 12.000000
     mol modcolor $repcount $moltop ColorID 7
     incr ::repcount
 
-    # add represe$repcount ntation for residues
+    # add representation for residues
     mol addrep $moltop
     mol modselect $repcount $moltop protein and resid 211
     mol modstyle $repcount $moltop Licorice 0.300000 12.000000 12.000000
