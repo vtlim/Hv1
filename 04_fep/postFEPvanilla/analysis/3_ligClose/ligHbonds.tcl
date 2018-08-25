44f7e41cf787f15960daa25f3c7e7f3f9e76bae9
Victoria Lim <lim.t.victoria@gmail.com>
2018-08-24 18:31:31 -0700

    results depend on wrap selection; set to protein

--- 04_fep/postFEPvanilla/analysis/3_ligClose/ligHbonds.tcl
+++ 04_fep/postFEPvanilla/analysis/3_ligClose/ligHbonds.tcl
@@ -2,13 +2,15 @@
 # Purpose: Quantify number of hbonds from (protein or water) to (given selection).
 # Usage:   vmdt -e ligHbonds.tcl -args selection,phrase,here inpsf mydcd1 mydcd2 [...]
 # Example: vmdt -e ligHbonds.tcl -args protein,and,resid,211 file.psf traj1.dcd
+# Notes:
+#   - Clean up in vi with: :%s/SegPROA-//g
 
 package require pbctools
 package require hbonds
 
 # ========================== Variables ========================= #
 
-set inskip 10
+set inskip 1
 
 set prelig [lindex $argv 0]
 set inpsf [lindex $argv 1]
@@ -24,7 +26,7 @@ mol new $inpsf
 foreach dcd $dcdlist {    ;# maybe alter the first step to read in if FEP bc 50 frames equil
     mol addfile $dcd first 0 last -1 step $inskip waitfor all
 }
-pbc wrap -compound fragment -center com -centersel "$mylig" -all
+pbc wrap -compound fragment -center com -centersel "protein" -all
 
 # evaluate hbonds
 hbonds -sel1 [atomselect top "protein or water"] -sel2 [atomselect top "$mylig"] -writefile yes -upsel yes -frames all -dist 3.5 -ang 40 -plot yes -log hbondsProtWat.log -writefile yes -outfile hbondsProtWat.dat -polar yes -DA both -type unique -detailout hbondsProtWat-details.dat
