50e430b2c2c48501a24cb7accf447d36d2f7973c
Victoria Lim <lim.t.victoria@gmail.com>
2018-09-05 11:45:47 -0700

    be able to extract selection in summary trajectory

--- 04_fep/postFEPvanilla/analysis/sumtraj.tcl
+++ 04_fep/postFEPvanilla/analysis/sumtraj.tcl
@@ -1,5 +1,6 @@
 
 
+# ======================================================================
 # Purpose: Create wrapped summary trajectory.
 #
 # Usage: vmdt -e sumtraj.tcl -args psffile outputName skip dcdfile1 lastframe1 [dcdfile2 lastframe2] [dcdfile3 lastframe3]
@@ -7,8 +8,14 @@
 #
 # Last frame can be obtained by subtracting one from catdcd output
 # Version: Oct 4 2017
+# ======================================================================
+
+# only write out specific selection; else comment this line
+#set extract "protein or resname GBI2"
+#set extract "protein"
 
 # for dopbc
+lappend auto_path /data12/cmf/limvt/tempotools/libs
 package require tempoUserVMD
 
 # read in command line arguments
@@ -17,17 +24,40 @@ set inpsf  [lindex $argv 0]
 set outputName [lindex $argv 1]
 set skip  [lindex $argv 2]
 
+# ======================================================================
 
+# load protein structure file
 mol new $inpsf
 
-set index 3
-set findex 4
+set index 3     ;# counter for file inputs (0=psf,1=output,2=skip)
+set findex 4    ;# counter for frame number as index+1 (TODO redundant)
 while {$index < $arglen} {
+
+    # get command line arguments from index
     set dcdfile [lindex $argv $index]
     set lastf [lindex $argv $findex]
+
+    # load and wrap trajectory
     dopbc -file $dcdfile -frames 0:$skip:$lastf -ref protein
     incr index 2
     incr findex 2
 }
 
-animate write dcd $outputName waitfor all
+
+# write output
+if {! [info exists extract] } {
+    animate write dcd $outputName waitfor all
+    exit
+} else {
+    # get psf basename
+    set fbasename [file rootname [file tail $outputName]]
+    # write dcd. if want subset of frames, use something like:   beg 0 end 9
+    animate write dcd $outputName       sel [atomselect top "$extract"] waitfor all
+    animate write psf "$fbasename.psf"  sel [atomselect top "$extract"]
+    animate write pdb "$fbasename.pdb"  sel [atomselect top "$extract"] beg 0 end 0
+    exit
+}
+
+
+
+
