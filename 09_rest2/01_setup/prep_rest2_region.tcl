a0a564f8bb03ebf47454064cbb2713a43b1b70d2
Victoria Lim <lim.t.victoria@gmail.com>
2018-11-14 11:04:04 -0800

    tcl script for rest2 prep

---
+++ 09_rest2/01_setup/prep_rest2_region.tcl
@@ -0,0 +1,21 @@
+
+# Purpose: Prepare PDB file for REST2 simulations
+# Usage: vmdt -e prep_spt.tcl -args input.pdb vmd,atom,selection output.pdb
+
+# read in command line arguments
+set inpdb  [lindex $argv 0]
+set vmd_sel_pre [lindex $argv 1]
+set outpdb  [lindex $argv 2]
+
+# process vmd selection
+set vmd_sel [split $vmd_sel_pre {,}]
+
+# do the job
+mol new $inpdb
+set all [atomselect top all]
+$all set occupancy 0.0
+set sel [atomselect top "$vmd_sel"]
+$sel set occupancy 1.0
+animate write pdb $outpdb
+exit
+
